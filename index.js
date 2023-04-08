5ce1d1b3a59cb29f75d7a6003b42d9c647ffc06b
Darseen <105995943+Darseen@users.noreply.github.com>
2023-04-08 22:10:14 +0300

    Changed port

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ app.use((err, req, res, next) => {
     res.render('error', { message, returnUrl: req.session.returnUrl });
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 })
